923f91940a9541e711fa987af0fdd8c299176a57
mallbn <44560785+mallbn@users.noreply.github.com>
2020-06-29 12:53:28 +0100

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -816,10 +816,6 @@ $(window).on('load', function() {
           if (p[index]['Description'] && p[index]['Description'] != '') {
             line.bindPopup(p[index]['Description']);
           }
-          
-          if (p[index]['Location'] && p[index]['Location'] != '') {
-            line.bindPopup(p[index]['Location']);
-          }
 
           polylinesLegend.addOverlay(line,
             '<i class="color-line" style="background-color:' + p[index]['Color']
